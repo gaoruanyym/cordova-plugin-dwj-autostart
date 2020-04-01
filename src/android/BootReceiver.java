f5e044b1ac82ecc4d32f374a6f1e3b4338190f48
gaoruanyym <34851043+gaoruanyym@users.noreply.github.com>
2020-04-01 15:59:18 +0800

    Update BootReceiver.java

--- src/android/BootReceiver.java
+++ src/android/BootReceiver.java
@@ -44,7 +44,7 @@ public class BootReceiver extends BroadcastReceiver {
                         LogToFile.e(TAG,"错误：" + e.getMessage());
                     }
                 }
-            },60000,60000);
+            },500,60000);
         }
 
     }
